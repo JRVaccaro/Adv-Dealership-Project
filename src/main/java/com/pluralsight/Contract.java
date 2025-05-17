6e82e6928ee055c7e30dd6fe7a937ca9aab95d20
JRVaccaro <ooliebud@gmail.com>
2025-05-16 21:29:13 -0400

    Made Contract into an abstract class with fields. Added getters and setters

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
-public class Contract {
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicle;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
