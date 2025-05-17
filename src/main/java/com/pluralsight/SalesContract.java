2d3d77489ca488ebec5c922911d1f93b2b7f247d
JRVaccaro <ooliebud@gmail.com>
2025-05-16 22:05:30 -0400

    Implemented SalesContract class as child class with fields, constructor, and getters/setters

--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -1,26 +1,75 @@
-@Override
-public double getTotalPrice() {
-    return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
-}
+package com.pluralsight;
 
-@Override
-public double getMonthlyPayment() {
-    int numberOfPayments = 0;
-    double interestRate = 0;
-    if (financeOption) {
-        if (getVehicleSold().getPrice() >= 10000) {
-            numberOfPayments = 48;
-            interestRate = 4.25 / 1200;
+public class SalesContract extends Contract {
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean financeOption;
+
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicle, double salesTaxAmount, double recordingFee, double processingFee, boolean financeOption) {
+        super(date, customerName, customerEmail, vehicle);
+        this.salesTaxAmount = salesTaxAmount;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.financeOption = financeOption;
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinanceOption() {
+        return financeOption;
+    }
+
+    public void setFinanceOption(boolean financeOption) {
+        this.financeOption = financeOption;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return getVehicle().getPrice() + salesTaxAmount + recordingFee + processingFee; //changed it from getVehicleSold()
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 0;
+        double interestRate = 0;
+        if (financeOption) {
+            if (getVehicle().getPrice() >= 10000) { //changed it from getVehicleSold()
+                numberOfPayments = 48;
+                interestRate = 4.25 / 1200;
+            } else {
+                numberOfPayments = 24;
+                interestRate = 5.25 / 1200;
+            }
+
+            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+            monthlyPayment = Math.round(monthlyPayment * 100);
+            monthlyPayment /= 100;
+            return monthlyPayment;
         } else {
-            numberOfPayments = 24;
-            interestRate = 5.25 / 1200;
+            return 0.0;
         }
-
-        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
-        monthlyPayment = Math.round(monthlyPayment * 100);
-        monthlyPayment /= 100;
-        return monthlyPayment;
-    } else {
-        return 0.0;
     }
 }
