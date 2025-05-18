ecef923e9467b788af4ea4c35575d2eed09a0bc0
JRVaccaro <ooliebud@gmail.com>
2025-05-17 20:19:52 -0400

    Replaced BufferedReader with BufferedWriter. Added header row to file, and formatted SalesContract data in pipe-seperated format

--- src/main/java/com/pluralsight/ContractDataManager.java
+++ src/main/java/com/pluralsight/ContractDataManager.java
@@ -1,18 +1,36 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ContractDataManager {
 
     public static void saveContract(Contract contract) {
-        try (BufferedReader br = new BufferedReader(new FileReader("contracts.csv"))) {
 
-
+        //creating a file object
+       File contractFile = new File("contracts.csv");
+        //Checking if file exists
+       boolean fileExists = contractFile.exists();
+       //checking if file is empty
+       boolean fileEmpty = !fileExists || contractFile.length() == 0;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("contracts.csv", true))) {
+            //if file is empty, write header row
+            if (fileEmpty){
+                writer.write("TYPE|DATE|NAME|VIN|MAKE|MODEL|TYPE|COLOR|ODOMETER|PRICE|SALES_TAX|RECORDING_FEE|FINANCE|MONTHLY_PAYMENT\n");
+            }
+            //checking if contract is a SalesContract instance
             if (contract instanceof SalesContract) {
                 SalesContract sales = (SalesContract) contract;
-
+                //Formating data in pipe-separated format
+                    writer.write("SALE|%s|%s|%s|%d|%d|%s|%s|%s|%s|%d|%.2f|%.2f|%.2f|%.2f|%.2f|%s|%.2f\n".formatted(
+                            sales.getDate(),
+                            sales.getCustomerName(),
+                            sales.getCustomerEmail(),
+                            sales.getVehicle().getVin(), sales.getVehicle().getYear(),
+                            sales.getVehicle().getMake(), sales.getVehicle().getModel(),
+                            sales.getVehicle().getVehicleType(), sales.getVehicle().getColor(),
+                            sales.getVehicle().getOdometer(), sales.getVehicle().getPrice(),
+                            sales.getSalesTaxAmount(), sales.getRecordingFee(), sales.getProcessingFee(),
+                            sales.getTotalPrice(), sales.isFinanceOption() ? "Yes" : "No", sales.getMonthlyPayment()));
 
             } else if (contract instanceof LeaseContract) {
                 LeaseContract lease = (LeaseContract) contract;
